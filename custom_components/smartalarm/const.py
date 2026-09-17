ee91eb669516688acca803938080fe18c3dcd357
esbart1 <101978607+esbart1@users.noreply.github.com>
2026-09-17 15:25:22 +0200

    Add SmartAlarm constants

---
+++ custom_components/smartalarm/const.py
@@ -0,0 +1,14 @@
+DOMAIN = "smartalarm"
+BASE_URL = "https://mijn.smartalarm.nl"
+DEFAULT_SCAN_INTERVAL = 5
+BASE_ID = "1429"
+
+STATE_AWAY = "away"
+STATE_HOME = "home"
+STATE_DISARM = "disarm"
+
+STATE_NAMES = {
+    STATE_AWAY: "Alarm aan",
+    STATE_HOME: "Alarm thuis aan",
+    STATE_DISARM: "Alarm uit",
+}
